a8dab2b03996ab4831108988c91ac2b10811a9db
syh5975 <112943024+syh5975@users.noreply.github.com>
2022-09-19 10:48:32 +0900

    finished chapter01

---
+++ Chapter02/c_printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+
+// 제어문자
+// + \n : 한줄 개행
+// + \t : 탭(8칸 스페이스)
+
+
+{
+      /*code*/
+     printf("HELLO\n");
+     printf("Yunho \t SEONG");
+
+     return 0;
+
+}
