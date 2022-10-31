123ca931158a76b2e922e8853ef9e9b92f1bf944
syh5975 <112943024+syh5975@users.noreply.github.com>
2022-10-31 10:39:46 +0900

    Add files via upload

---
+++ chapter06/while.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+
+/**
+ * 반복문 
+ *   - 조건을 만족하는 동안 반복해서 실행
+ *
+ * 
+ *  1) while문 - 반복횟수 모름
+ *  2) for문 - 반복횟수가 정해진 경우
+*/
+int main(void) {
+    int a = 1;
+
+    while(a < 10) {
+        a = a*2;
+    }
+    printf("a : %d\n", a);
+
+}
